03bf740ff6bd676f60e5584806a4bb7e2c158984
iam-player <iampl4y3r@gmail.com>
2023-07-06 11:22:49 +0200

    feaute: dynamic removing tasks from list

--- assets/scripts/todo.js
+++ assets/scripts/todo.js
@@ -1,14 +1,14 @@
 let input = document.getElementById("input");
 let add = document.getElementById("add");
+let remove = document.getElementById("remove");
 
 let tasksList = document.getElementById("list");
 let tasks = [];
 
 const addTaskToList = input => {
     tasksList.value = "";
-    tasksList.innerHTML += '<li>'+input+'</li>';
+    tasksList.innerHTML += '<li>'+input+'<span>X</span></li>';
     tasks.push(input);
-    console.log(tasks);
 };
 
 add.addEventListener("click", (e) => {
@@ -19,4 +19,16 @@ add.addEventListener("click", (e) => {
     } else {
         throw new Error ("User input is empty! Cannot add a task.");
     }
+});
+
+tasksList.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if(e.target.tagName === "LI"){
+        console.log("li");
+    } else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        console.log("removing");
+    }
+    
 });
